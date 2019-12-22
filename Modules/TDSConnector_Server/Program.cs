7cb26e4164428644b3496ae288509a18d0284be0
emre1702 <emre1702@live.de>
2019-12-22 11:09:12 +0100

    Update project paths, remove useless file, add TDSConnector_Server Module (init)

---
+++ Modules/TDSConnector_Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace TDSConnector
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CreateHostBuilder().Build().Run();
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
