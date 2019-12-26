72273368a2341190944b429de07f2f47c73b5c72
emre1702 <emre1702@live.de>
2019-12-26 11:39:39 +0100

    Add support for "message to user"

---
+++ Modules/TDSConnectorServerAssembly/Services/MessageToUserService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Grpc.Core;
+
+namespace TDSConnectorServerAssembly
+{
+    public class MessageToUserService : MessageToUser.MessageToUserBase
+    {
+
+        public override async Task<MessageToUserRequestReply> Send(MessageToUserRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var client = Program.DiscordClient;
+
+                var guild = client.GetGuild(request.GuildId);
+                if (guild is null)
+                    return new MessageToUserRequestReply { ErrorMessage = $"The guild with Id {request.GuildId} does not exist." };
+
+                SocketGuildUser? target = GetUser(guild, request.UserIdOrDiscriminiator);
+
+                if (target is null)
+                    return new MessageToUserRequestReply { ErrorMessage = null };
+
+                var privateChat = await target.GetOrCreateDMChannelAsync();
+                await privateChat.SendMessageAsync(request.Text);
+
+                return new MessageToUserRequestReply { ErrorMessage = null };
+            }
+            catch (Exception ex)
+            {
+                return new MessageToUserRequestReply
+                {
+                    ErrorMessage = ex.GetBaseException().Message
+                };
+            }
+        }
+
+        public override async Task<MessageToUserRequestReply> SendEmbed(EmbedToUserRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var client = Program.DiscordClient;
+
+                var guild = client.GetGuild(request.GuildId);
+                if (guild is null)
+                    return new MessageToUserRequestReply { ErrorMessage = $"The guild with Id {request.GuildId} does not exist." };
+
+                SocketGuildUser? target = GetUser(guild, request.UserIdOrDiscriminiator);
+                if (target is null)
+                    return new MessageToUserRequestReply { ErrorMessage = null };
+
+                var embedBuilder = new EmbedBuilder();
+
+                if (!string.IsNullOrEmpty(request.Title))
+                    embedBuilder.WithTitle(request.Title);
+
+                if (!string.IsNullOrEmpty(request.Author))
+                    embedBuilder.WithAuthor(request.Author);
+
+                embedBuilder.WithTimestamp(DateTimeOffset.UtcNow);
+
+                foreach (var field in request.Fields)
+                    embedBuilder.AddField(field.Name, field.Value);
+
+                if (request.ColorR != -1 || request.ColorG != -1 || request.ColorB != -1)
+                    embedBuilder.WithColor(request.ColorR != -1 ? request.ColorR : 255, request.ColorG != -1 ? request.ColorG : 255, request.ColorB != -1 ? request.ColorB : 255);
+
+                var privateChat = await target.GetOrCreateDMChannelAsync();
+                await privateChat.SendMessageAsync(embed: embedBuilder.Build());
+
+                return new MessageToUserRequestReply { ErrorMessage = null };
+            }
+            catch (Exception ex)
+            {
+                return new MessageToUserRequestReply
+                {
+                    ErrorMessage = ex.GetBaseException().Message
+                };
+            }
+        }
+
+        private static SocketGuildUser? GetUser(SocketGuild guild, string userIdOrDiscriminiator)
+        {
+            try
+            {
+                if (ulong.TryParse(userIdOrDiscriminiator, out ulong userId))
+                {
+                    return guild.GetUser(userId);
+                }
+                
+                return guild.Users.FirstOrDefault(u => !u.IsBot && u.Discriminator.Equals(userIdOrDiscriminiator, StringComparison.InvariantCultureIgnoreCase));
+            } 
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- Modules/TDSConnectorServerAssembly/Startup.cs
+++ Modules/TDSConnectorServerAssembly/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,6 +29,7 @@ namespace TDSConnectorServerAssembly
             {
                 endpoints.MapGrpcService<RAGEServerStatsService>();
                 endpoints.MapGrpcService<MessageToChannelService>();
+                endpoints.MapGrpcService<MessageToUserService>();
 
                 endpoints.MapGet("/", async context =>
                 {
