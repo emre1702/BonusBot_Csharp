c559ea36dbc65c19bc2c5ae060b272a3d5870fa0
emre1702 <emre1702@live.de>
2020-01-04 23:24:17 +0100

    Add TDS module for !confirmtds command + TDS-V connection (as client)

---
+++ Modules/TDSAssembly/Commands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace TDSAssembly
+{
+    partial class TDSModule
+    {
+        [Command("ConfirmTDS"), Alias("ConfirmIdentity", "ConfirmUserId")]
+        public async Task ConfirmUserId()
+        {
+            try
+            {
+                var reply = await _connectorClient.UsedCommandAsync(new UsedCommandRequest
+                {
+                    UserId = Context.User.Id,
+                    Command = "confirmtds"
+                });
+
+                if (!string.IsNullOrEmpty(reply.Message))
+                    await ReplyAsync(reply.Message);
+            }
+            catch (Exception ex)
+            {
+                await ReplyAsync("Confirming failed:" + Environment.NewLine + ex.GetBaseException().Message);
+            }
+           
+        }
+    }
+}
---
+++ Modules/TDSAssembly/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BonusBot.Common.ExtendedModules;
+using Discord.Commands;
+using Grpc.Net.Client;
+using static TDSAssembly.BBCommand;
+
+namespace TDSAssembly
+{
+    public partial class TDSModule : CommandBase
+    {
+        private static BBCommandClient _connectorClient; 
+
+        protected override void BeforeExecute(CommandInfo command)
+        {
+            base.BeforeExecute(command);
+
+            if (_connectorClient is { })
+                return;
+
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var grpcChannel = GrpcChannel.ForAddress("http://localhost:5001");
+            _connectorClient = new BBCommandClient(grpcChannel);
+        }
+    }
+}
